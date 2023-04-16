9be61412b679ed2a7c885329c1dff33e2bb5ba06
Kev <sulpeum@gmail.com>
2023-04-16 15:29:14 -0400

    chore: removed opacity

--- src/components/CaseStudy/CaseParagraph.tsx
+++ src/components/CaseStudy/CaseParagraph.tsx
@@ -5,7 +5,7 @@ type Props = {
 export const CaseParagraph = ({ children }: Props) => {
   return (
     <div className="divide-y divide-stone-300 py-10 lg:max-w-[50ch]">
-      <p className=" font-inter text-sm leading-[2.2] opacity-80">{children}</p>
+      <p className=" font-inter text-sm leading-[2.2] text-caseText">{children}</p>
     </div>
   )
 }
